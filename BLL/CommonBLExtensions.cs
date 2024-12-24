859407b5961bbc8b172eeea58dd02ef2dbc08457
Hi-sari <srh7259@gmail.com>
2024-12-24 17:37:56 +0200

    נסיון להוספת פונקציית הרחבה

---
+++ BLL/CommonBLExtensions.cs
@@ -0,0 +1,18 @@
+﻿using BL_API;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    public static class CommonBLExtensions
+    {
+        public static void AddDependencies (this IServiceCollection ser)
+        {
+            ser.AddScoped<ICustomerBL , CustomerBL> (); 
+        }
+    }
+}
